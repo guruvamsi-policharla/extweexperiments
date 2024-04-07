2e7babafb27cc57b7606445dce2298f2782d9911
Guru Vamsi Policharla <guruvamsi.policharla@gmail.com>
2024-04-07 16:49:12 -0700

    modified params.py

--- network/params.py
+++ network/params.py
@@ -21,10 +21,16 @@ if LOCAL:
     old_addrs = ['localhost' + ':' + str(50000 + n) for n in range(N[-1])]
     new_addrs = ['localhost' +  ':' + str(50000 + N[-1] + n) for n in range(N[-1])]
 else:
+    # read addresses from the file host_sockets. the first n/2 are old_addrs and the rest are new_addrs
+    with open('host_sockets', 'r') as f:
+        addrs = f.readlines()
+        old_addrs = addrs[:int(len(addrs) / 2)]
+        new_addrs = addrs[int(len(addrs) / 2):]
+
     #old_addrs = ['node' + str(n) + ':' + '50050' for n in range(N[-1])]
     #new_addrs = ['node' + str(N[-1] + n) +  ':' + '50050' for n in range(N[-1])]
-    old_addrs = ['192.168.1.' + str(n + 1) + ':' + '50050' for n in range(N[-1])]
-    new_addrs = ['192.168.1.' + str(N[-1] + n + 1) +  ':' + '50050' for n in range(N[-1])]
+    # old_addrs = ['192.168.1.' + str(n + 1) + ':' + '50050' for n in range(N[-1])]
+    # new_addrs = ['192.168.1.' + str(N[-1] + n + 1) +  ':' + '50050' for n in range(N[-1])]
 
 resultsfile = 'experiment_results.txt'
 

742b3703f1f4d4f436f10e8917f22b0872e44215
Guru Vamsi Policharla <guruvamsi.policharla@gmail.com>
2024-04-07 12:39:46 -0700

    trying to stop vms

--- gcp/stop_vms.sh
+++ gcp/stop_vms.sh
@@ -1,4 +1,7 @@
 names=$(for i in $(seq 1 $(($n))); do echo "node-$i "; done)
-gcloud compute instances stop $names \
-    --zone us-central1-a\
-    --project "batch-threshold" \
+
+# Stop VMs
+for name in $names; do
+  gcloud compute instances stop $name --zone us-central1-a --project "batch-threshold" &
+done
+wait

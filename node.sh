f50c45aee6a9a22d6b886abf64d05d6c5d0fc06c
Guru Vamsi Policharla <guruvamsi.policharla@gmail.com>
2024-04-07 10:43:49 -0700

    updated node.sh

--- node.sh
+++ node.sh
@@ -1,35 +1,49 @@
-# cd /local/repository/
-
-sudo apt install git
-git clone https://github.com/guruvamsi-policharla/extweexperiments.git
-cd extweexperiments
-
 echo "Installing Required Packages"
-
-echo "Installing pip"
-sudo apt update
-sudo apt --assume-yes install python3-pip
 
 echo "Installing gmp"
 sudo apt-get --assume-yes install libgmp-dev
 sudo apt-get --assume-yes install swig
 
+echo "Installing pip"
+sudo apt update
+sudo apt --assume-yes install python3-pip
+sudo apt --assume-yes install python3-venv
+
+python3 -m venv dpss-env
+source dpss-env/bin/activate
+
 echo "Installing Python packages"
-sudo python3 -m pip install numpy
+pip3 install numpy grpcio grpcio-tools pytz
 
-echo "GRPC"
-sudo -H pip3 install --upgrade pip
-sudo python3 -m pip install grpcio
-sudo -H pip3 install grpcio-tools
-
-echo "Additional"
-sudo pip3 install pytz
-
-echo "Building"
+sudo apt install git
+git clone https://github.com/guruvamsi-policharla/extweexperiments.git
+cd extweexperiments
 sudo make
 
-echo "Starting node${1}:${2}"
-cd network
-python3 node.py -a "node${1}:${2}" -i $1
+# echo "Installing pip"
+# sudo apt update
+# sudo apt --assume-yes install python3-pip
+
+# echo "Installing gmp"
+# sudo apt-get --assume-yes install libgmp-dev
+# sudo apt-get --assume-yes install swig
+
+# echo "Installing Python packages"
+# sudo python3 -m pip install numpy
+
+# echo "GRPC"
+# sudo -H pip3 install --upgrade pip
+# sudo python3 -m pip install grpcio
+# sudo -H pip3 install grpcio-tools
+
+# echo "Additional"
+# sudo pip3 install pytz
+
+# echo "Building"
+# sudo make
+
+# echo "Starting node${1}:${2}"
+# cd network
+# python3 node.py -a "node${1}:${2}" -i $1
 
 

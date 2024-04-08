2babb09e900b7752b22dc8015afd13b27b86b9e8
Guru Vamsi Policharla <guruvamsi.policharla@gmail.com>
2024-04-08 14:06:05 -0700

    added a rate parameter

--- gcp/network-delay-start.sh
+++ gcp/network-delay-start.sh
@@ -4,15 +4,16 @@ trap "kill 0" EXIT
 
 DELAY_TIME=200ms
 JITTER_TIME=10ms
+RATE=10mbit
 
-sudp tc qdisc add dev ens4 root netem delay $DELAY_TIME $JITTER_TIME
+sudo tc qdisc add dev ens4 root netem delay $DELAY_TIME $JITTER_TIME rate $RATE
 
 n=$(<n)
 names=$(for i in $(seq 0 $((n-1))); do echo "node-$i "; done)
 
 for name in $names
 do
-  gcloud compute ssh $name --zone=us-central1-a --internal-ip --command="sudo tc qdisc add dev ens4 root netem delay $DELAY_TIME $JITTER_TIME" &
+  gcloud compute ssh $name --zone=us-central1-a --internal-ip --command="sudo tc qdisc add dev ens4 root netem delay $DELAY_TIME $JITTER_TIME rate $RATE" &
 done
 wait
 

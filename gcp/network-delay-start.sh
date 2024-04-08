25e8f2b82b17b856199aaeea4ebb85adbaa91ce7
Guru Vamsi Policharla <guruvamsi.policharla@gmail.com>
2024-04-08 13:45:48 -0700

    testing delays

--- gcp/network-delay-start.sh
+++ gcp/network-delay-start.sh
@@ -2,14 +2,17 @@ set -ex
 trap "exit" INT TERM
 trap "kill 0" EXIT
 
-sudp tc qdisc add dev ens4 root netem delay 100ms 10ms
+DELAY_TIME=200ms
+JITTER_TIME=10ms
+
+sudp tc qdisc add dev ens4 root netem delay $DELAY_TIME $JITTER_TIME
 
 n=$(<n)
 names=$(for i in $(seq 0 $((n-1))); do echo "node-$i "; done)
 
 for name in $names
 do
-  gcloud compute ssh $name --zone=us-central1-a --internal-ip --command="sudo tc qdisc add dev ens4 root netem delay 100ms 10ms" &
+  gcloud compute ssh $name --zone=us-central1-a --internal-ip --command="sudo tc qdisc add dev ens4 root netem delay $DELAY_TIME $JITTER_TIME" &
 done
 wait
 
